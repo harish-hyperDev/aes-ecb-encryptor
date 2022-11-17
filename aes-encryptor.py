407547f22b5fad58af3c3cf5a551af854f144911
harish-hyperDev <coolprogrammer.777@gmail.com>
2022-11-17 14:34:59 +0530

    Encrypted string will be copied to clipboard automatically

--- aes-encryptor.py
+++ aes-encryptor.py
@@ -1,6 +1,7 @@
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Cipher import AES
 import base64
+import pyperclip as clip
 
 BLOCK_SIZE = 16 # Bytes
 
@@ -13,7 +14,9 @@ msg = cipher.encrypt(pad(input_msg, BLOCK_SIZE))
 bmsg = base64.b64encode(msg)
 
 print("Encrypted String: "+bmsg.decode())
+clip.copy(bmsg.decode())
 
+print("---Encrypted String is copied to your clipboard---");
 #decipher = AES.new(key, AES.MODE_ECB)
 #msg_dec = decipher.decrypt(msg)
 #print(unpad(msg_dec, BLOCK_SIZE).decode())
