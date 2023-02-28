7b22cb244f3698c505b5b0b4bbaf6a69599112d4
harish-hyperDev <coolprogrammer.777@gmail.com>
2023-02-28 19:29:46 +0530

    added 'hidden text' and 'hidden key' functionality

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ from Crypto.Util.Padding import pad, unpad
 from Crypto.Cipher import AES
 import base64
 import pyperclip as clip
+import getpass # for hiding text on type
+import sys # for getting sys.argv
 
-BLOCK_SIZE = 16 # Bytes
+BLOCK_SIZE = 16 # 16 Bytes indicating AES-128 ECB
 
-input_msg = bytes(input("Enter Text: "), encoding='utf-8')
-key = bytes(input("Enter Key: "),encoding='utf-8')
+input_msg = None
+key = None
+
+try:
+    if sys.argv[1] == "--hidden":
+        input_msg = bytes(getpass.getpass("Enter Text: "), encoding='utf-8')
+        key = bytes(getpass.getpass("Enter Key: "), encoding='utf-8')
+
+except:
+    if input_msg is None and key is None:   # if argv[1] is not hidden(i.e, None)
+        input_msg = bytes(input("Enter Text: "), encoding='utf-8')
+        key = bytes(input("Enter Key: "),encoding='utf-8')
 
 cipher = AES.new(key, AES.MODE_ECB)
 
